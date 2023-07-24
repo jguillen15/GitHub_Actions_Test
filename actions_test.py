b2c1eceafc98a8a1d990322c48dbe341f590138d
jguillen15 <47996074+jguillen15@users.noreply.github.com>
2023-07-24 10:51:29 -0700

    Create actions_test.py

---
+++ actions_test.py
@@ -0,0 +1,18 @@
+import argparse
+
+def main():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('number')
+    args_dict = vars(parser.parse_args())
+    number_retrieved = int(args_dict['number'])
+
+    if number_retrieved > 10:
+        print("Oops!")
+        exit()
+        print("This should not be printed")
+    else:
+        print("All good :)")
+
+if __name__ == '__main__':
+    main()
